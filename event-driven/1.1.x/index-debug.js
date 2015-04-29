95f86b4b750fc682976c53da590caec5f4e3d510
Heero.Luo <heeroluo@gmail.com>
2015-04-29 14:32:04 +0800

    方法写到prototype上

--- event-driven/1.1.x/index-debug.js
+++ event-driven/1.1.x/index-debug.js
@@ -1,6 +1,6 @@
 /*!
  * JRaiser 2 Javascript Library
- * event-driven - v1.1.0 (2015-04-27T10:32:48+0800)
+ * event-driven - v1.1.0 (2015-04-29T14:31:32+0800)
  * http://jraiser.org/ | Released under MIT license
  */
 define(function(require, exports, module) { 'use strict';
@@ -22,45 +22,43 @@ var base = require('base/1.1.x/'), EventArg = require('./event-arg');
  */
 return base.createClass(function() {
 	// 存放回调函数
-	var handlers = { };
-
+	this.__eventHandlers = { };
+}, {
 	/**
 	 * 触发事件
 	 * @method trigger
 	 * @for EventDriven
 	 * @param {String} type 事件类型
 	 * @param {Object} [props] 事件属性
+	 * @param {Any} [thisObj] 监听函数的this，默认为当前对象
 	 * @return {Object} 事件参数
 	 */
-	this.trigger = function(type, props) {
-		var theHandlers = handlers[type], e = new EventArg(type, props);
-		if (theHandlers) {
-			for (var i = 0; i < theHandlers.length; i++) {
-				theHandlers[i].call(this, e);
+	trigger: function(type, props, thisObj) {
+		var handlers = this.__eventHandlers[type], e = new EventArg(type, props);
+		if (handlers) {
+			for (var i = 0; i < handlers.length; i++) {
+				handlers[i].call(thisObj || this, e);
 			}
 		}
 
 		return e;
-	};
+	},
 
 	/**
 	 * 注册事件监听
 	 * @method on
 	 * @for EventDriven
 	 * @param {String} type 事件类型
-	 * @param {Function|Array} handler 处理函数。多个处理函数以数组传入
+	 * @param {Function} handler 处理函数
 	 * @return {Object} 当前对象
 	 */
-	this.on = function(type, handler) {
+	on: function(type, handler) {
+		var handlers = this.__eventHandlers;
 		handlers[type] = handlers[type] || [ ];
-		if ( base.isArray(handler) ) {
-			base.merge(handlers[type], handler);
-		} else {
-			handlers[type].push(handler);
-		}
+		handlers[type].push(handler);
 
 		return this;
-	};
+	},
 
 	/**
 	 * 移除事件监听
@@ -70,25 +68,31 @@ return base.createClass(function() {
 	 * @param {Function} [handler] 处理函数。如不指定，则取消指定事件类型的所有监听
 	 * @return {Object} 当前对象
 	 */
-	this.off = function(type, handler) {
-		if (!arguments.length) {
-			handlers = { };
-		} else if (!handler) {
-			delete handlers[type];
-		} else {
-			var theHandlers = handlers[type];
-			if (theHandlers) {
-				for (var i = theHandlers.length - 1; i >= 0; i--) {
-					if (theHandlers[i] === handler) {
-						theHandlers.splice(i, 1);
+	off: function(type, handler) {
+		switch (arguments.length) {
+			case 0:
+				this.__eventHandlers = { };
+				break;
+
+			case 1:
+				delete this.__eventHandlers[type];
+				break;
+
+			case 2:
+				var handlers = this.__eventHandlers[type];
+				if (handlers) {
+					for (var i = handlers.length - 1; i >= 0; i--) {
+						if (handlers[i] === handler) {
+							handlers.splice(i, 1);
+						}
 					}
+					if (!handlers.length) { delete this.__eventHandlers[type]; }
 				}
-				if (!theHandlers.length) { delete handlers[type]; }
-			}
+				break;
 		}
 
 		return this;
-	};
+	}
 });
 
 });
--- event-driven/1.1.x/index.js
+++ event-driven/1.1.x/index.js
@@ -6,7 +6,7 @@
 define("event-driven/1.1.x/event-arg",null,function(t,e,n){"use strict";function i(){return!1}function a(){return!0}var r=t("base/1.1.x/");return r.createClass(function(t,e){var n=this;r.extend(n,e),n.type=t,n.timeStamp=+new Date},{preventDefault:function(){this.isDefaultPrevented=a},stopPropagation:function(){this.isPropagationStopped=a},isDefaultPrevented:i,isPropagationStopped:i})});
 /*!
  * JRaiser 2 Javascript Library
- * event-driven - v1.1.0 (2015-04-27T10:32:48+0800)
+ * event-driven - v1.1.0 (2015-04-29T14:31:32+0800)
  * http://jraiser.org/ | Released under MIT license
  */
-define("event-driven/1.1.x/",["base/1.1.x/"],function(e,t,r){"use strict";var n=e("base/1.1.x/"),i=e("./event-arg");return n.createClass(function(){var e={};this.trigger=function(t,r){var n=e[t],s=new i(t,r);if(n)for(var f=0;f<n.length;f++)n[f].call(this,s);return s},this.on=function(t,r){return e[t]=e[t]||[],n.isArray(r)?n.merge(e[t],r):e[t].push(r),this},this.off=function(t,r){if(arguments.length)if(r){var n=e[t];if(n){for(var i=n.length-1;i>=0;i--)n[i]===r&&n.splice(i,1);n.length||delete e[t]}}else delete e[t];else e={};return this}})});
+define("event-driven/1.1.x/",["base/1.1.x/"],function(e,t,n){"use strict";var r=e("base/1.1.x/"),s=e("./event-arg");return r.createClass(function(){this.__eventHandlers={}},{trigger:function(e,t,n){var r=this.__eventHandlers[e],a=new s(e,t);if(r)for(var i=0;i<r.length;i++)r[i].call(n||this,a);return a},on:function(e,t){var n=this.__eventHandlers;return n[e]=n[e]||[],n[e].push(t),this},off:function(e,t){switch(arguments.length){case 0:this.__eventHandlers={};break;case 1:delete this.__eventHandlers[e];break;case 2:var n=this.__eventHandlers[e];if(n){for(var r=n.length-1;r>=0;r--)n[r]===t&&n.splice(r,1);n.length||delete this.__eventHandlers[e]}}return this}})});
