821642131b4989f29a356e5cb6f4199136a332e0
Heero.Law <heeroluo@gmail.com>
2016-04-24 13:20:47 +0800

    增加Promise.race方法；代码优化

--- promise/1.0.x/index-debug.js
+++ promise/1.0.x/index-debug.js
@@ -1,6 +1,6 @@
 /*!
  * JRaiser 2 Javascript Library
- * promise - v1.0.0 (2016-04-21T18:04:28+0800)
+ * promise - v1.0.0 (2016-04-24T13:18:38+0800)
  * http://jraiser.org/ | Released under MIT license
  */
 define(function(require, exports, module) { 'use strict';
@@ -23,18 +23,18 @@ var logError = typeof console !== 'undefined' && console.error ?
 
 // 判断某个对象是否支持then方法
 function isThenable(obj) {
-	return obj && typeof obj.then === 'function' && obj.then.length === 2;
+	return obj && typeof obj.then === 'function';
 }
 
 
-// Promise对象的三种状态
+// Promise的三种状态
 var STATUS_PENDING = 0,
 	STATUS_FULFILLED = 1,
 	STATUS_REJECTED = 2;
 
 
 /**
- * 地区选择组件
+ * ES6 Promise的兼容实现
  * @class Promise
  * @constructor
  * @exports
@@ -42,7 +42,7 @@ var STATUS_PENDING = 0,
  */
 var Promise = base.createClass(function(executor) {
 	if (typeof executor !== 'function') {
-		throw new Error('Executor must be a function');
+		throw new Error('Promise executor must be a function');
 	}
 
 	var t = this;
@@ -51,16 +51,16 @@ var Promise = base.createClass(function(executor) {
 	t._status = STATUS_PENDING;
 
 	t._pendings = { };
-	// 存放操作成功的回调函数
+	// 存放promise被解决后的回调函数
 	t._pendings[STATUS_FULFILLED] = [ ];
-	// 存放操作失败的回调函数
+	// 存放promise被拒绝后的回调函数
 	t._pendings[STATUS_REJECTED] = [ ];
 
 	try {
 		executor(function(value) {
 			t._settle(STATUS_FULFILLED, value);
-		}, function(error) {
-			t._settle(STATUS_REJECTED, error);
+		}, function(reason) {
+			t._settle(STATUS_REJECTED, reason);
 		});
 	} catch (e) {
 		t._settle(STATUS_REJECTED, e);
@@ -82,7 +82,7 @@ var Promise = base.createClass(function(executor) {
 		delete t._pendings;
 	},
 
-	// 添加回调函数到指定的状态
+	// 添加回调函数
 	_listen: function(status, cb) {
 		var t = this;
 		if (t._status === STATUS_PENDING) {
@@ -96,13 +96,13 @@ var Promise = base.createClass(function(executor) {
 	 * 指定当前promise状态确定后的操作
 	 * @method then
 	 * @for Promise
-	 * @param {Function} onFulfilled 当前promise操作成功后调用的函数
-	 * @param {Function} onRejected 当前promise操作失败后调用的函数
-	 * @return {Promise} 表示回调操作结果的promise
+	 * @param {Function} onFulfilled 当前promise被解决后调用的函数
+	 * @param {Function} onRejected 当前promise被拒绝后调用的函数
+	 * @return {Promise} 以回调函数执行结果确定状态的promise
 	 */
 	then: function(onFulfilled, onRejected) {
 		var t = this;
-		
+
 		return new Promise(function(resolve, reject) {
 			t._listen(STATUS_FULFILLED, function() {
 				var value = t._value;
@@ -110,8 +110,9 @@ var Promise = base.createClass(function(executor) {
 					try {
 						value = onFulfilled(value);
 					} catch (e) {
-						logError(value);
+						logError(e);
 						reject(e);
+						return;
 					}
 				}
 				if ( isThenable(value) ) {
@@ -127,8 +128,9 @@ var Promise = base.createClass(function(executor) {
 					try {
 						value = onRejected(value);
 					} catch (e) {
-						logError(value);
+						logError(e);
 						reject(e);
+						return;
 					}
 					if ( isThenable(value) ) {
 						value.then(resolve, reject);
@@ -143,11 +145,11 @@ var Promise = base.createClass(function(executor) {
 	},
 
 	/**
-	 * 指定当前promise在操作失败后的操作
-	 * @method then
+	 * 指定当前promise被拒绝后执行的操作
+	 * @method catch
 	 * @for Promise
-	 * @param {Function} onRejected 当前promise操作失败后调用的函数
-	 * @return {Promise} 表示回调操作结果的promise
+	 * @param {Function} onRejected 当前promise被拒绝后调用的函数
+	 * @return {Promise} 以回调函数执行结果确定状态的promise
 	 */
 	'catch': function(onRejected) { return this.then(null, onRejected); }
 });
@@ -155,12 +157,12 @@ var Promise = base.createClass(function(executor) {
 // 静态方法
 base.extend(Promise, {
 	/**
-	 * 返回一个表示所有指定Promise操作结果的Promise
+	 * 返回一个表示所有指定promise操作结果的promise
 	 * @method all
 	 * @for Promise
 	 * @static
-	 * @param {Array<Promise>} promises 所有指定的Promise
-	 * @return {Promise} 表示所有指定的Promise操作结果的Promise
+	 * @param {Array<Promise>} promises 指定的promise
+	 * @return {Promise} promise
 	 */
 	all: function(promises) {
 		var count = promises.length;
@@ -181,11 +183,27 @@ base.extend(Promise, {
 	},
 
 	/**
-	 * 生成操作成功状态的promise
+	 * 返回一个promise，这个promise根据第一个确定状态的指定promise而确定状态
+	 * @method race
+	 * @for Promise
+	 * @static
+	 * @param {Array<Promise>} promises 指定的promise
+	 * @return {Promise} promise
+	 */
+	race: function(promises) {
+		return new Promise(function(resolve, reject) {
+			promises.forEach(function(promise, i) {
+				promise.then(resolve, reject);
+			});
+		});
+	},
+
+	/**
+	 * 返回一个以指定值解析的promise
 	 * @method resolve
 	 * @for Promise
 	 * @static
-	 * @param {Any} value 操作成功的值
+	 * @param {Any} value 指定值。可以是定值、promise或者thenable
 	 * @return {Promise} promise
 	 */
 	resolve: function(value) {
@@ -195,16 +213,16 @@ base.extend(Promise, {
 	},
 
 	/**
-	 * 生成操作失败状态的promise
+	 * 返回一个拒绝状态的promise
 	 * @method reject
 	 * @for Promise
 	 * @static
-	 * @param {Any} value 失败原因
+	 * @param {Any} reason 拒绝原因
 	 * @return {Promise} promise
 	 */
-	reject: function(value) {
+	reject: function(reason) {
 		return new Promise(function(resolve, reject) {
-			reject(value);
+			reject(reason);
 		});
 	}
 });
