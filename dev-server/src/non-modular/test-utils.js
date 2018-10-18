8df7c66052a5bd71e8d06cebdf72b87e363e2fe7
jiangminxi <jiangminxi@ibeiliao.com>
2018-10-18 10:42:54 +0800

    代码优化

--- dev-server/src/non-modular/test-utils.js
+++ dev-server/src/non-modular/test-utils.js
@@ -2,23 +2,23 @@ window.testUtils = {};
 (function(testUtils) {
 	testUtils.getErrByType = function(value) {
 		var type = '',
-			valueDisplay = JSON.stringify(value);;
+			valueDisplay = JSON.stringify(value);
 		if (typeof value === 'object') {
-			switch(Object.prototype.toString.call(value)) {
+			switch (Object.prototype.toString.call(value)) {
 				case '[object Array]':
 					type = 'Array';
 					break;
 				default:
-					type = 'Object'
+					type = 'Object';
 			}
 		} else {
 			type = typeof value;
 		}
 		return '非法值-' + type + '类型-' + valueDisplay;
-	}
-	testUtils.parseDom = function(arg) {
-	　　 var objE = document.createElement("div");
-	　　 objE.innerHTML = arg;
-	　　 return objE.childNodes;
+	};
+	testUtils.parseDOM = function(html) {
+		var div = document.createElement('div');
+		div.innerHTML = html;
+		return div.childNodes;
 	};
 })(window.testUtils);
