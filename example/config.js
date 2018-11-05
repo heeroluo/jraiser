83ba68682f25d9bde0b4e6329f2f772b28ed5953
Heero.Law <heeroluo@gmail.com>
2018-11-05 09:42:56 +0800

    移除旧版本代码

--- example/config.js
+++
@@ -1,9 +0,0 @@
-bowljs.config({
-	libPath: '../lib',
-	appPath: './app',
-	preload: [
-		Function.prototype.bind ? '' : 'compatibility/es5-shim',
-		window.JSON ? '' : 'compatibility/json2',
-		window.localStorage ? '' : 'compatibility/localstorage'
-	]
-});
