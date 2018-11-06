dc4bd72c7375abfbe0775058f8a7a6251d6580cb
Heero.Law <heeroluo@gmail.com>
2018-11-06 11:04:59 +0800

    代码优化

--- dev-server/src/unit-test/web-storage/1.1/main.js
+++ dev-server/src/unit-test/web-storage/1.1/main.js
@@ -22,15 +22,15 @@ QUnit.test('读写', function(assert) {
 QUnit.test('过期时间', function(assert) {
 	var done = assert.async();
 
-	var now = new Date();
-	webStorage.set('testExpires', '1', new Date(now.getFullYear() + 1, 0, 1));
-	webStorage.set('testExpires', '1', new Date(now.getFullYear() - 1, 0, 1));
-	assert.equal(webStorage.get('testExpires'), null, '先设为未过期，再设为过期');
-
 	webStorage.set('testExpires', '1', '1 sec');
 	assert.strictEqual(webStorage.get('testExpires'), '1', '过期前');
 	setTimeout(function() {
 		assert.equal(webStorage.get('testExpires'), null, '过期后');
 		done();
 	}, 1200);
+
+	var now = new Date();
+	webStorage.set('testExpires2', '1', new Date(now.getFullYear() + 1, 0, 1));
+	webStorage.set('testExpires2', '1', new Date(now.getFullYear() - 1, 0, 1));
+	assert.equal(webStorage.get('testExpires2'), null, '先设为未过期，再设为过期');
 });
