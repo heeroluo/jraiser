feff5742d5fcad3597566f45afd5ae63da2bf76d
Heero.Law <heeroluo@gmail.com>
2018-11-24 16:20:35 +0800

    新增构建 npm 包任务

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,16 @@ const path = require('path');
 const bowljsCLI = require('bowljs-cli');
 
 
-gulp.task('clearDist', function() {
-	fse.removeSync('./dist');
-	fse.mkdirpSync('./dist');
+// # 以 CMD 规范构建模块
+
+// ## 清理旧文件
+gulp.task('clearCMD', function() {
+	fse.removeSync('./dist-cmd');
+	fse.mkdirpSync('./dist-cmd');
 });
 
-gulp.task('copyJS', ['clearDist'], function(cb) {
+// ## 复制模块，并以 define 包装
+gulp.task('wrapJSModule', ['clearCMD'], function(cb) {
 	pump([
 		gulp.src('./src/**/*.js'),
 		gulpModify({
@@ -27,22 +31,55 @@ gulp.task('copyJS', ['clearDist'], function(cb) {
 				}
 			}
 		}),
-		gulp.dest('./dist/')
+		gulp.dest('./dist-cmd/')
 	], cb)
 });
 
-gulp.task('compileJS', ['copyJS'], () => {
+// ## 调用 Bowljs CLI 构建
+gulp.task('buildCMD', ['wrapJSModule'], function() {
 	bowljsCLI.build(
-		path.resolve('./dist'),
+		path.resolve('./dist-cmd'),
 		path.resolve('./package.settings')
 	);
 });
 
 
+// # NPM 模块构建
+
+// ## 清理旧文件
+gulp.task('clearNPM', function() {
+	fse.removeSync('./dist-npm');
+	fse.mkdirpSync('./dist-npm');
+});
+
+// ## 复制模块
+gulp.task('copyJSToNPM', ['clearNPM'], function(cb) {
+	pump([
+		gulp.src('./src/**/*.js'),
+		gulp.dest('./dist-npm/')
+	], cb);
+});
+
+// ## 复制 package.json ，并删除开发部分
+gulp.task('copyPkgJSON', ['clearNPM'], function() {
+	const pkg = require('./package');
+	delete pkg.devDependencies;
+	delete pkg.scripts;
+	fse.writeFileSync('./dist-npm/package.json', JSON.stringify(pkg, null, 2));
+});
+
+// ## 构建
+gulp.task('buildNPM', ['copyJSToNPM', 'copyPkgJSON']);
+
+
+// # 生成文档
+
+// ## 清理文档
 gulp.task('clearDoc', function() {
 	fse.removeSync('./docs/api/3.0');
 });
 
+// ## 生成文档
 gulp.task('genDoc', ['clearDoc'], function() {
 	bowljsCLI.genDoc(
 		path.resolve('./dist'),
