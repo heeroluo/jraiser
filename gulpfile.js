18fd0765ecb30f4481545d6ddf1d3898fbe73108
Heero.Law <heeroluo@gmail.com>
2020-01-17 11:39:19 +0800

    升级到 Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
+const path = require('path');
 const gulp = require('gulp');
 const gulpModify = require('gulp-modify');
 const pump = require('pump');
 const fse = require('fs-extra');
-const path = require('path');
 const bowljsCLI = require('bowljs-cli');
 
 
 // # 以 CMD 规范构建模块
 
-// ## 清理旧文件
-gulp.task('clearCMD', function() {
+// ## 清理旧的 CMD 构建文件
+function clearCMD(cb) {
 	fse.removeSync('./dist-cmd');
 	fse.mkdirpSync('./dist-cmd');
-});
+	cb();
+}
 
-// ## 复制模块，并以 define 包装
-gulp.task('wrapJSModule', ['clearCMD'], function(cb) {
+// ## 模块增加 define 包装
+function wrapJSModule(cb) {
 	pump([
 		gulp.src('./src/**/*.js'),
 		gulpModify({
@@ -33,64 +34,76 @@ gulp.task('wrapJSModule', ['clearCMD'], function(cb) {
 		}),
 		gulp.dest('./dist-cmd/')
 	], cb)
-});
+}
 
 // ## 调用 Bowljs CLI 构建
-gulp.task('buildCMD', ['wrapJSModule'], function() {
+function buildJSModule(cb) {
 	bowljsCLI.build(
 		path.resolve('./dist-cmd'),
 		path.resolve('./package.settings')
 	);
-});
+	cb();
+}
+
+exports.buildCMD = gulp.series(clearCMD, wrapJSModule, buildJSModule);
 
 
 // # NPM 模块构建
 
 // ## 清理旧文件
-gulp.task('clearNPM', function() {
+function clearNPM(cb) {
 	fse.removeSync('./dist-npm');
 	fse.mkdirpSync('./dist-npm');
-});
+	cb();
+}
 
 // ## 复制模块
-gulp.task('copyJSToNPM', ['clearNPM'], function(cb) {
+function copyJSToNPM(cb) {
 	pump([
 		gulp.src('./src/**/*.js'),
 		gulp.dest('./dist-npm/')
 	], cb);
-});
+}
 
 // ## 复制 package.json ，并删除开发部分
-gulp.task('copyPkgJSON', ['clearNPM'], function() {
+function buildPkgJSON(cb) {
 	const pkg = require('./package');
 	delete pkg.devDependencies;
 	delete pkg.scripts;
 	fse.writeFileSync('./dist-npm/package.json', JSON.stringify(pkg, null, 2));
-});
+	cb();
+}
 
 // ## 复制 README.md
-gulp.task('copyReadme', ['clearNPM'], function(cb) {
+function copyReadme(cb) {
 	pump([
 		gulp.src('./README.md'),
 		gulp.dest('./dist-npm/')
 	], cb);
-});
+}
 
-// ## 构建
-gulp.task('buildNPM', ['copyJSToNPM', 'copyPkgJSON', 'copyReadme']);
+exports.buildNPM = gulp.series(
+	clearNPM,
+	copyJSToNPM,
+	buildPkgJSON,
+	copyReadme
+);
 
 
 // # 生成文档
 
 // ## 清理文档
-gulp.task('clearDoc', function() {
+function clearDoc(cb) {
 	fse.removeSync('./docs/api/3.0');
-});
+	cb();
+}
 
-// ## 生成文档
-gulp.task('genDoc', ['clearDoc'], function() {
+function genDoc(cb) {
 	bowljsCLI.genDoc(
 		path.resolve('./dist'),
 		path.resolve('./document.settings')
 	)
-});
+	cb();
+}
+
+exports.genDoc = gulp.series(clearDoc, genDoc);
